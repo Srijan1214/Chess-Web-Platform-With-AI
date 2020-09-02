81da77933969b98775325b260ec719a98a567006
Srijan1214 <srijan1214@gmail.com>
2020-09-02 17:49:13 -0400

    completed documentation for makeMove.js

--- src/classic chess api/makeMove.js
+++ src/classic chess api/makeMove.js
@@ -1,6 +1,38 @@
+/**/
+/* 
+	 * FILE DESCRIPTION: 
+	 *	This file contains functions that facilitate making piece moves given a particular
+	 *	position.
+	 *	They also help in editing the GameBoard to manipulate which piece is where.
+*/
+/**/
 import { PIECES, PCEINDEX, FROMSQ, TOSQ, MFLAGEP, COLOURS, MFLAGCA, SQUARES, CAPTURED, MFLAGPS, PROMOTED,
 	PieceVal, PieceCol, Kings, CastlePerm, PiecePawn} from "./defs.js"
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.ClearPiece() - Clear the piece to an empty piece at a particular square
+
+SYNOPSIS : ClearPiece(a_square_120)
+
+DESCRIPTION 
+			Clear the piece to an empty piece at a particular square.
+			Reflects the necessary changes in all the arrays and variables that use that square.
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/6/2020
+
+*/
+/**/
 export function ClearPiece (a_square_120) {
 	let pce = this.m_pieces[a_square_120];
 	let col = PieceCol[pce];
@@ -23,7 +55,31 @@ export function ClearPiece (a_square_120) {
 	this.m_pList[PCEINDEX(pce, t_pceNum)] =
 		this.m_pList[PCEINDEX(pce, this.m_pceNum[pce])];
 }
+/* ClearPiece(ClearPiece)*/
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.AddPiece() - Adds a piece to a particular square.
+
+SYNOPSIS : AddPiece(a_square_120, a_piece)
+
+DESCRIPTION 
+			Add a piece to a square given the piece type and 120 Board square number.
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/12/2020
+
+*/
+/**/
 export function AddPiece (a_square_120, a_piece) {
 	let col = PieceCol[a_piece];
 
@@ -34,7 +90,35 @@ export function AddPiece (a_square_120, a_piece) {
 	this.m_pList[PCEINDEX(a_piece, this.m_pceNum[a_piece])] = a_square_120;
 	this.m_pceNum[a_piece]++;
 };
+/* AddPiece(a_square_120, a_piece)*/
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.MovePiece() - Move the pieces from one square to another.
+
+SYNOPSIS : MovePiece(a_from, a_to)
+			a_from: The 120_Board array square number from which the move is made.
+			a_to: The 120_Board array square number to which the move is made.
+
+DESCRIPTION 
+			Move the pieces from one square to another.
+			Reflects necessary changes in all arrays and variables.
+			Does not care if move is legal or not.
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/12/2020
+
+*/
+/**/
 export function MovePiece (a_from, a_to) {
 	let index = 0
 	let pce = this.m_pieces[a_from]
@@ -52,7 +136,33 @@ export function MovePiece (a_from, a_to) {
 		}
 	}
 }
+/* MovePiece(a_from, a_to) */
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.MakeMove() - Executes the move in the board given the move number.
+
+SYNOPSIS : MakeMove(a_move_number)
+			a_move_number -> The 32 bit move number encapsulating all the information needed for a move.
+
+DESCRIPTION 
+			Executes the move in the board given the move number.
+			Returns false if invalid move
+
+RETURNS : true is legal move. False if the move is invalid.
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/12/2020
+
+*/
+/**/
 export function MakeMove (a_move_number){
 	let from = FROMSQ(a_move_number)
 	let to = TOSQ(a_move_number)
@@ -145,7 +255,33 @@ export function MakeMove (a_move_number){
 
 	return true
 }
+/* MakeMove(a_move_number) */
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.TakeMove() - Goes back the game history by one step.
+
+SYNOPSIS : TakeMove()
+
+DESCRIPTION 
+			Goes back the game history by one step.
+			Takes back the latest move.
+			Should not be called if in the starting position.
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/12/2020
+
+*/
+/**/
 export function TakeMove () {
 	this.m_hisPly--;
 	this.m_ply--;
@@ -207,3 +343,4 @@ export function TakeMove () {
 		);
 	}
 };
+/* TakeMove() */
