899e8ce7e2c515e6e6537ade27c90741b7260637
Srijan1214 <srijan1214@gmail.com>
2020-09-02 17:24:55 -0400

    Completed documentation for io.js

--- src/classic chess api/io.js
+++ src/classic chess api/io.js
@@ -1,9 +1,64 @@
+/**/
+/* 
+	 * FILE DESCRIPTION: Functions helpful in debugging. They print was is going on to the console.
+*/
+/**/
 import { FROMSQ, TOSQ, PROMOTED, PIECES, FilesBrd, RanksBrd, RankChar, FileChar, PieceKnight, PieceRookQueen, PieceBishopQueen,  } from "./defs.js"
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.PrSq() - Returns the square in terms of the standard chess coordinates.
+
+SYNOPSIS : PrSq(a_square_120)
+			a_square_120 -> The 120 Board index.
+
+DESCRIPTION 
+			Returns the square in terms of the standard chess coordinates.
+
+RETURNS : The coordinate string (e.g 'a1', 'h4') given by the 120 board index.
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 06/29/2020
+
+*/
+/**/
 export function PrSq(a_square_120) {
 	return (FileChar[FilesBrd[a_square_120]] + RankChar[RanksBrd[a_square_120]])
 }
+/* PrSq(a_square_120)*/
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.PrMove() - Returns the chess move in terms of the standard chess notation.
+
+SYNOPSIS : PrMove(a_move_number)
+			a_move_number -> The 32 bit move number encapsulating all the information needed for a move.
+DESCRIPTION 
+			Returns the chess move in terms of the standard chess notation.
+			e.g. Returns 'a1' for an input of 21. 
+
+
+RETURNS : the move in string format.
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 06/29/2020
+
+*/
+/**/
 export function PrMove (a_move_number) {
 	let MvStr;
 
@@ -35,7 +90,32 @@ export function PrMove (a_move_number) {
 	}
 	return MvStr;
 };
+/* PrMove(a_move_number)*/
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.PrintMoveList() - Console.logs all the legal moves in a given position.
+
+SYNOPSIS : PrintMoveList()
+
+DESCRIPTION 
+			Console.logs all the legal moves in a given position.
+			Does not take checks into consideration.
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 06/29/2020
+
+*/
+/**/
 export function PrintMoveList() {
 	let index
 	let move
@@ -48,3 +128,4 @@ export function PrintMoveList() {
 		num++
 	}
 }
+/* PrintMoveList()*/
