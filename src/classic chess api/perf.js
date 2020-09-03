216c13f37d97f9f04cac20ac945dbc90dc2ccf5c
Srijan1214 <srijan1214@gmail.com>
2020-09-03 04:14:56 -0400

    Added documentation for perf.js

--- src/classic chess api/perf.js
+++ src/classic chess api/perf.js
@@ -1,3 +1,12 @@
+/**/
+/* 
+	 * FILE DESCRIPTION: 
+	 * This file contains functions that help check for errors in the move list generation.
+	 * I can call the GenerateMove() funtion in tricky positions(using SetFen()) with many edge cases
+	 * and these functions will generate the total number of nodes. If everything is correct 
+	 * the value should be the same for as the one gotten through another chess tool that I used.
+*/
+/**/
 import { PrMove } from "./io.js"
 import { MakeMove, TakeMove } from "./makeMove.js"
 import { GenerateMoves } from "./movegen.js"
@@ -5,6 +14,30 @@ import GameBoard from "./board.js"
 
 let perft_leafNodes
 
+
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.Perft()- Gets the total number of legal moves in all sub variations up till a certain depth. 
+
+SYNOPSIS : Perft(a_depth)
+			a_depth -> The depth to which the number of nodes are calculated.
+
+DESCRIPTION 
+			Gets the total number of legal moves in all sub variations up till a certain depth. 
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/15/2020
+
+*/
+/**/
 function Perft(a_depth) {
 	if (a_depth == 0) {
 		perft_leafNodes++
@@ -28,7 +61,33 @@ function Perft(a_depth) {
 
 	return
 }
+/* Perft(a_depth) */
 
+
+
+
+
+
+ /**/
+/*
+NAME : GameBoard.PerftTest(a=)- Gets the total number of legal moves up till a certain depth and prints it. 
+
+SYNOPSIS : PerftTest(a_depth)
+			a_depth -> The depth to which the number of nodes are calculated.
+
+DESCRIPTION 
+			Gets the total number of legal moves in all sub variations up till a certain depth. 
+			Prints it to the console.
+			If anything is wrong in the implementation, a wrong number will be printed.
+
+RETURNS : NOTHING
+
+AUTHOR : Srijan Prasad Joshi
+
+DATE : 07/15/2020
+
+*/
+/**/
 export const PerftTest = (a_depth) => {
 	GameBoard.PrintBoard();
 	console.log("Starting Test To Depth:" + a_depth);
@@ -60,3 +119,4 @@ export const PerftTest = (a_depth) => {
 
 	return;
 };
+/* PerftTest (a_depth) */
