3c75ae2391417b68dc8a00078cef35afd16b8f5d
Srijan1214 <srijan1214@gmail.com>
2020-09-03 21:32:03 -0400

    added documentation for the search.js file.

--- src/AI src files/search.js
+++ src/AI src files/search.js
@@ -1,3 +1,11 @@
+/**/
+/* 
+	 * FILE DESCRIPTION: 
+	 * This file contains the AI class that contains all the logic to implement the AI using
+	 * the alpha-beta search method and quiescence search. They utilize the evaluate function implemented in 
+	 * the evaluate.js file.
+*/
+/**/
 import * as eval_necessities from "./evaluate.js"
 import * as pvtable_necessities from "./pvtable.js"
 import {
@@ -17,7 +25,28 @@ import {
 	MFLAGEP,
 } from "../classic chess api/defs.js"
 
+// The AI class containing all the functions needed for a chess AI.
 export default class AI {
+	 /**/
+	/*
+	NAME : GameBoard.constructor - Checks if there were any errors during the initializations.
+
+	SYNOPSIS : GameBoard.constructor(GameBoard)
+				GameBoard -> a GameBoard object gotten from the classic\ chess\ api/GameBoard class. This object
+							is used to simulate the chess and an perform the alpha-beta search on its position.
+
+	DESCRIPTION 
+				Binds all the functions defined in other classes to the class. Also, binds the constant piece
+				tables defined in evaluate.js.
+
+	RETURNS : NOTHING
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/09/2020
+
+	*/
+	/**/
 	constructor(GameBoard) {
 		this.SearchController = {}
 		this.GameBoard = GameBoard
@@ -37,11 +66,61 @@ export default class AI {
 		
 		this.SetGameBoard(GameBoard)
 	}
+	/* .(GameBoard()()() */
 
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.SetGameBoard() - Sets the member GameBoard object to the argument passed.
+
+	SYNOPSIS : SetGameBoard(a_GameBoard)
+
+	DESCRIPTION 
+				Sets the member GameBoard object to the argument passed.
+
+	RETURNS : NOTHING
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/18/2020
+
+	*/
+	/**/
 	SetGameBoard(a_GameBoard) {
 		this.GameBoard = a_GameBoard
 	}
+	/* SetGameBoard(a_GameBoard) */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.PickNextMove() - Puts the best move in the beginning of the m_moveList and m_moveScores array.
+
+	SYNOPSIS : PickNextMove(a_move_number)
+				a_move_number -> Index of the current best move inside the  m_moveList and m_moveScores array.
+
+	DESCRIPTION 
+				Puts the best move in the beginning of the m_moveList and m_moveScores array.
+				This improves move ordering and speeds up the AI considerably. (approx. by a factor of 3-4)
+
+	RETURNS : NOTHING
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	PickNextMove(a_move_number) {
 		let index = 0
 		let bestScore = -1
@@ -59,16 +138,43 @@ export default class AI {
 		}
 
 		if (bestNum !== a_move_number) {
+			// swap the previously best score with the current move score in the m_moveScores array.
 			let temp = this.GameBoard.m_moveScores[a_move_number]
 			this.GameBoard.m_moveScores[a_move_number] = this.GameBoard.m_moveScores[bestNum]
 			this.GameBoard.m_moveScores[bestNum] = temp
 
+			// swap the previously best move with the current move move in the m_moveList array.
 			temp = this.GameBoard.m_moveList[a_move_number]
 			this.GameBoard.m_moveList[a_move_number] = this.GameBoard.m_moveList[bestNum]
 			this.GameBoard.m_moveList[bestNum] = temp
 		}
 	}
+	/* PickNextMove(a_move_number) */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.ClearPvTable() - Resets the principal variation table so that no moves are calculated.
+
+	SYNOPSIS : ClearPvTable()
+
+	DESCRIPTION 
+				Resets the principal variation table so that no moves are calculated.
+
+	RETURNS : NOTHING
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	ClearPvTable() {
 		let index
 
@@ -77,7 +183,33 @@ export default class AI {
 			this.GameBoard.m_PvTable[index].poskey = 0
 		}
 	}
+	/* ClearPvTable() */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.CheckUp() - Stops the searching in the AI if it exceeds the time limit set.
+
+	SYNOPSIS : CheckUp()
+
+	DESCRIPTION 
+				Stops the searching in the AI if it exceeds the time limit set.
+				This is done by setting the SearchController.stop to true.
+
+	RETURNS : NOTHING
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	CheckUp() {
 		if (
 			Date.now() - this.SearchController.start >
@@ -86,7 +218,32 @@ export default class AI {
 			this.SearchController.stop = true
 		}
 	}
+	/* CheckUp() */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.IsRepetition() - Checks if position is draw by repetition.
+
+	SYNOPSIS : IsRepetition()
+
+	DESCRIPTION 
+				Checks if position is draw by repetition.
+
+	RETURNS : True if game is a draw by repetition. False if no.
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	IsRepetition() {
 		let index = 0
 
@@ -100,7 +257,41 @@ export default class AI {
 			}
 		}
 	}
+	/* IsRepetition() */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.Quiescence() - Performs a shallow quiescence search on the position.
+
+	SYNOPSIS : Quiescence(a_alpha, a_beta)
+				a_alpha -> The alpha value evaluated till the current node in the alpha-beta tree.
+				a_beta -> The beta value evaluated till the current node in the alpha-beta tree.
+
+	DESCRIPTION 
+				Performs a shallow quiescence search on the position.
+				This search is called by the alpha-beta search in the leaf nodes.
+				The quiescence search is better than using a raw evaluation function.
+				This search just searches the volatile final positions a few moves more until the evaluation
+				stabilizers. This type of search is rife in many board game AIs.
+				https://en.wikipedia.org/wiki/Quiescence_search
+
+	RETURNS : An integer value representing how good the quiescence search thinks the current position is.
+				A large negative value means the position is bad for the side to move while a large positive
+				value means that the position is good for the side to move.
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	Quiescence(a_alpha, a_beta) {
 		// Check Time Up
 		if ((this.SearchController.nodes & 2047) === 0) {
@@ -183,7 +374,43 @@ export default class AI {
 
 		return a_alpha
 	}
+	/* Quiescence(a_alpha, a_beta) */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.AlphaBeta() - Performs a standard alpha-beta pruning search on the current position.
+
+	SYNOPSIS : AlphaBeta(a_alpha, a_beta, a_depth)
+				a_alpha -> The alpha value evaluated till the current node in the alpha-beta tree.
+				a_beta -> The beta value evaluated till the current node in the alpha-beta tree.
+				a_depth -> The depth up till which to perform the alpha-beta search.
+
+	DESCRIPTION 
+				Performs a standard alpha-beta search on the current position.
+				An alpha-beta search is the optimized version of the min-max search.
+				In a min-max search, the algorithm searches each and every move, while in an alpha-beta
+				search the algorithm will stop search a particular variation if it doesn't make any sense to
+				look at this variation and other compelling variations were already searched.
+				https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
+
+
+	RETURNS : An integer value representing how good the alpha-beta search thinks the current position is.
+				A large negative value means the position is bad for the side to move while a large positive
+				value means that the position is good for the side to move.
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	AlphaBeta(a_alpha, a_beta, a_depth) {
 		if (a_depth <= 0) {
 			return this.Quiescence(a_alpha, a_beta)
@@ -309,7 +536,32 @@ export default class AI {
 
 		return a_alpha
 	}
+	/* AlphaBeta(a_alpha, a_beta, a_depth) */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.ClearForSearch() - Resets the necessary variables to start analyzing a position.
+
+	SYNOPSIS : ClearForSearch()
+
+	DESCRIPTION 
+				Resets the necessary variables to start analyzing a position from scratch.
+
+	RETURNS : NOTHING
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	ClearForSearch() {
 		let index = 0
 
@@ -329,7 +581,36 @@ export default class AI {
 		this.SearchController.start = Date.now()
 		this.SearchController.stop = false
 	}
+	/* ClearForSearch() */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.SearchPosition() - Makes the AI search the position and give out the best move.
+
+	SYNOPSIS : SearchPosition()
+
+	DESCRIPTION 
+				Makes the AI search the position and give out the best move.
+				Executes the alpha-beta search on the position.
+
+	RETURNS : An object giving all the details about the best move.
+				The details are the move's from and to square in standard string format,
+				weather if the move is a castling move, promotion move or a capture move,
+				the 32 bit number encapsulating the entire move.
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	SearchPosition() {
 		let bestMove = NOMOVE
 		let bestScore = -INFINITE
@@ -391,8 +672,34 @@ export default class AI {
 			enPass: enPass
 		}
 	}
+	/* SearchPosition() */
 
+
+
+
+
+
+
+
+	 /**/
+	/*
+	NAME : GameBoard.GetBestScore() - Gets the latest score that the AI evaluated from the SearchPosition().
+
+	SYNOPSIS : GetBestScore()
+
+	DESCRIPTION 
+				Gets the latest score that the AI evaluated from the SearchPosition().
+
+	RETURNS : The position evaluation integer value.
+
+	AUTHOR : Srijan Prasad Joshi
+
+	DATE : 08/11/2020
+
+	*/
+	/**/
 	GetBestScore() {
 		return this.SearchController.bestScore
 	}
+	/* GetBestScore() */
 }
