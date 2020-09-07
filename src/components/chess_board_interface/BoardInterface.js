568f0342c8d5741828ec974e2d9693c29d160618
Srijan1214 <srijan1214@gmail.com>
2020-09-07 15:14:48 -0400

    Changed some url's for the final demo.

--- src/components/chess_board_interface/BoardInterface.js
+++ src/components/chess_board_interface/BoardInterface.js
@@ -261,7 +261,9 @@ class BoardInterface extends React.Component {
 		const loadImage = (piece) => new Promise(resolve => {
 			let img = new Image();
 			// img.src = "https://short-newt-45.serverless.social/resources/image/piece/" + piece;
-			img.src = "http://localhost:3001/resources/image/piece/" + piece;
+			// img.src = "http://localhost:3001/resources/image/piece/" + piece
+			img.src = "https://wonderful-donkey-87.serverless.social/resources/image/piece/" + piece
+			
 			img.draggable = "true"
 			img.name = piece
 			img.onload = function () {
